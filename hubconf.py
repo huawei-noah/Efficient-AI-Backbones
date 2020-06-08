f0c636bfdc976297286b2fd18e44efcaaed89e1e
iamhankai <kaihana@163.com>
2020-06-09 00:44:14 +0800

    update pytorch hub

--- hubconf.py
+++ hubconf.py
@@ -3,7 +3,7 @@ import torch
 from pytorch.ghostnet import ghostnet
 
 
-state_dict_url = 'https://github.com/huawei-noah/ghostnet/blob/master/pytorch/models/state_dict_93.98.pth'
+state_dict_url = 'https://github.com/huawei-noah/ghostnet/raw/master/pytorch/models/state_dict_93.98.pth'
 
 
 def ghostnet_1x(pretrained=False, **kwargs):
@@ -15,3 +15,4 @@ def ghostnet_1x(pretrained=False, **kwargs):
 	  if pretrained:
 	  	  state_dict = torch.hub.load_state_dict_from_url(state_dict_url, progress=True)
 	  	  model.load_state_dict()
+	  return model
